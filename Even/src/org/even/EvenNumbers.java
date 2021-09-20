f4579f9de460cdbed5a966c00d3eb648a522c2ee
vaishnavi <vaishnavi@vaishu>
2021-09-20 14:58:20 +0530

    Even commit

---
+++ Even/src/org/even/EvenNumbers.java
@@ -0,0 +1,19 @@
+package org.even;
+
+	public class EvenNumbers
+	{  
+	public static void main(String args[])   
+	{  
+	int number=100;  
+	System.out.print("List of even numbers from 1 to "+number+": ");  
+	for (int i=1; i<=number; i++)   
+	{  
+	if (i%2==0)   
+	{  
+	System.out.print(i + " ");  
+	}  
+	}  
+	}  
+	}  
+
+
