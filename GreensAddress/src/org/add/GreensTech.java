63c4b5c986b3244b76792db6acb9cf434b4a3643
vaishnavi <vaishnavi@vaishu>
2021-09-20 16:08:14 +0530

    Green commit

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,38 @@
+package org.add;
+
+import java.util.Scanner;
+
+public class GreensTech
+{
+  public  Object greensOmr()
+  {
+	  return null;
+	  
+  }
+  @Override
+public String toString() 
+  {
+	return "GreensTech [greensOmr()=" + greensOmr() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+			+ ", toString()=" + super.toString() + "]";
+}
+public static void main (String [] args)
+  {
+	System.out.println("greensomr()");
+	  int Id=98;
+		 System.out.println("Id");
+		 System.out.println("Id is :" +Id);
+		  
+		 long number= 886580980;
+		System.out.println("Number:" +number);
+		 
+	
+		 double orderNo= 25.00;
+		 System.out.println("OrderNo is:"+orderNo);
+		 
+		
+		 String ordertype= "Tech";
+		 System.out.println("Ordertype:"+ordertype);
+		 
+	  }
+  }
+
